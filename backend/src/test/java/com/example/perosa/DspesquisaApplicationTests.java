bdc83e38df3394a7055edd7d4a45c32bbe1a6a4b
perosa100 <patrickperosapp@gmail.com>
2020-09-14 11:00:24 -0300

    Project Created

---
+++ backend/src/test/java/com/example/perosa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

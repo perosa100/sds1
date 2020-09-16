5f5783f4ca6df2e58ea209dcbb7bc1a2427e533c
perosa100 <patrickperosapp@gmail.com>
2020-09-16 15:36:12 -0300

    Criando Página de Listagem dos registros

--- front/src/pages/Records/index.tsx
+++ front/src/pages/Records/index.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './styles.css'
+import { RecordsResponse } from './types'
+import api from './../../config/api'
+import { formatDate } from './helpers'
+import Pagination from './Pagination/index'
+import { Link } from 'react-router-dom'
 
 const Records: React.FC = () => {
-  return <h1>Records</h1>
+  const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>()
+  const [activePage, setActivePage] = useState(0)
+
+  useEffect(() => {
+    api.get(`records?linesPerPage=12&page=${activePage}`).then((response) => {
+      setRecordsResponse(response.data)
+    })
+  }, [activePage])
+
+  const handlePageChange = (index: number) => {
+    setActivePage(index)
+  }
+
+  return (
+    <div className="page-container">
+      <div className="filters-container records-actions">
+        <Link to="/charts">
+          <button className="action-filters">Ver Gráfico</button>
+        </Link>
+      </div>
+
+      <table className="records-table" cellPadding="0" cellSpacing="0">
+        <thead>
+          <tr>
+            <th>INSTANTE</th>
+            <th>NOME</th>
+            <th>IDADE</th>
+            <th>PLATAFORMA</th>
+            <th>GÊNERO</th>
+            <th>TÍTULO DO GAME</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recordsResponse?.content.map((record) => (
+            <tr key={record.id}>
+              <td>{formatDate(record.moment)}</td>
+              <td>{record.name}</td>
+              <td>{record.age}</td>
+              <td className="text-secondary">{record.platform}</td>
+              <td>{record.genreName}</td>
+              <td className="text-primary">{record.gameTitle}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination
+        activePage={activePage}
+        goToPage={handlePageChange}
+        totalPages={recordsResponse?.totalPages}
+      />
+    </div>
+  )
 }
 
 export default Records

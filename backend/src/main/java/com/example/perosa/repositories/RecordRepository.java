9e49ad5f55e4ad2356a96ec4bcf0ddd6c8fa51e9
perosa100 <patrickperosapp@gmail.com>
2020-09-16 02:19:44 -0300

    GET Records

--- backend/src/main/java/com/example/perosa/repositories/RecordRepository.java
+++ backend/src/main/java/com/example/perosa/repositories/RecordRepository.java
@@ -1,13 +1,22 @@
 package com.example.perosa.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-
-import com.example.perosa.entities.Genre;
 import com.example.perosa.entities.Record;
 
 @Repository
-public interface RecordRepository extends JpaRepository<Record, Long>{
+public interface RecordRepository extends JpaRepository<Record, Long> {
+
+	@Query("SELECT obj FROM Record obj WHERE "
+			+ "(coalesce(:min,null) IS NULL OR obj.moment >= :min) AND"
+			+ "(coalesce(:max,null) IS NULL OR obj.moment <= :max)")
+	Page<Record> findByMoments(Instant min, Instant max, Pageable pageRequest);
 
 }
--- backend/src/main/java/com/example/perosa/resources/RecordResource.java
+++ backend/src/main/java/com/example/perosa/resources/RecordResource.java
@@ -1,15 +1,21 @@
 package com.example.perosa.resources;
 
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.perosa.dto.GameDTO;
 import com.example.perosa.dto.RecordDTO;
 import com.example.perosa.dto.RecordInsertDTO;
 
@@ -18,16 +24,35 @@ import com.example.perosa.services.RecordService;
 @RestController
 @RequestMapping(value = "/records")
 public class RecordResource {
-	
 
 	@Autowired
 	private RecordService service;
-	
-	
+
 	@PostMapping
-	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto){
+	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto) {
 		RecordDTO newDTO = service.insert(dto);
-		
 		return ResponseEntity.ok().body(newDTO);
 	}
+
+	@GetMapping
+	public ResponseEntity<Page<RecordDTO>> findAll(@RequestParam(value = "min", defaultValue = "") String min,
+			@RequestParam(value = "max", defaultValue = "") String max,
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+
+		Instant minDate = ("".equals(min)) ? null : Instant.parse(min);
+		Instant maxDate = ("".equals(max)) ? null : Instant.parse(max);
+
+		if (linesPerPage == 0) {
+			linesPerPage = Integer.MAX_VALUE;
+		}
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		Page<RecordDTO> list = service.findByMoments(minDate, maxDate, pageRequest);
+		return ResponseEntity.ok().body(list);
+	}
+
 }
